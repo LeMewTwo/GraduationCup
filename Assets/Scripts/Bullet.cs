f35eed63b5bb63b43b29baa3562ac1639794a83a
Chris Lemieux <>
2020-02-17 10:59:08 -0600

    initial changes mon feb 17 10:50am

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bullet : MonoBehaviour
 {
-
+    //base speed for bullet
     public float speed = 30;
 
     private Rigidbody2D rigidBody;
@@ -12,21 +13,53 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //get reference to the Rigidbody for the bullet
         rigidBody = GetComponent<Rigidbody2D>();
 
+        //when bullet is created, it will move upward at the "speed"
         rigidBody.velocity = Vector2.up * speed;
     }
 
+    //everytime the bullet collides it will call this
     void OnTriggerEnter2D(Collider2D col)
     {
+        //if bullet hits a wall then it destroy the bullet
         if(col.tag == "Wall")
         {
             Destroy(gameObject);
         }
+
+        //if bullet hits enemy, then increase score and destroy bullet and enemy
+        if(col.tag == "Enemy")
+        {
+            SoundManager.Instance.PlayOneShot(SoundManager.Instance.enemyDies);
+
+            IncreaseScoreText();
+
+            Destroy(gameObject);
+
+            Destroy(col.gameObject, 0);
+        }
     }
 
+    //when bullet isn't used will call this and make invisible
     void OnBecomeInvisible()
     {
         Destroy(gameObject);
+    }
+
+    //increased the score in the text UI when called
+    void IncreaseScoreText()
+    {
+        //find the text Score UI component
+        var scoreTextComp = GameObject.Find("Score").GetComponent<Text>();
+
+        //get string store in the text and covert into a int
+        int score = int.Parse(scoreTextComp.text);
+
+        score += 25;
+
+        //covert the int score back into a string to update the text UI
+        scoreTextComp.text = score.ToString();
     }
 }
--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
 {
+
+    private Rigidbody2D rigidBody;
+
+    //speed for the bullet
+    public float speed = 30;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //get reference to Rigidbody for the bullet
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        //when bullet is called it will down at the speed
+        rigidBody.velocity = Vector2.down * speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    //when bullet hits something will call, object hit is passed as a parameter
+    void OnTriggerEnter2D(Collider2D col)
     {
-        
+        //if enemyBullet hits a wall it will destroy the bullet
+        if(col.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+
+        //if bullet hits player then destroy enemyBullet and Player
+        if(col.gameObject.tag == "Player")
+        {
+            //Play sound when player hit
+            SoundManager.Instance.PlayOneShot(SoundManager.Instance.playerDies);
+
+            //Destroy the player
+            Destroy(col.gameObject);
+
+            //Destroy the bullet
+            Destroy(gameObject);
+
+        }
+    }
+
+    //when bullet isn't used will call this and make invisible
+    void OnBecomeInvisible()
+    {
+        Destroy(gameObject);
     }
 }
