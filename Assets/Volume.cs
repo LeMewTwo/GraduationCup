f5e86bd71bafea2572cbc99245daa1405712bd24
Nishma <nismaluitel@gmail.com>
2020-04-12 17:17:35 -0500

    Added music and Volume adjustments

--- Assets/Volume.cs
+++ Assets/Volume.cs
@@ -5,9 +5,34 @@ using UnityEngine.Audio;
 
 public class Volume : MonoBehaviour
 {
-    public AudioMixer audioMixer;
-    public void SetVolume(float volume)
+    //Reference to Audio Scource component
+    private AudioSource audioSrc;
+
+    //Music volume variable that will be modified by dragging
+    //slider knob
+    private float musicVolume = 1f;
+
+    void Start()
     {
-        audioMixer.SetFloat("volume", volume);
+        //Assign Audio Source component to control it
+        audioSrc = GetComponent<AudioSource>();
     }
+
+    //Update is called once per team
+    void Update()
+    {
+        //Setting volume option of Audio Source to be equal to musicVolume
+        audioSrc.volume = musicVolume;
+    }
+
+    //Method that is called by slider game onject 
+    //This method takes vol value passed by slider
+    //and sets it as musicValue
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+
+
+
 }
